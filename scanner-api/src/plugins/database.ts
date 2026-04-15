8cdb0f2f07ca69cf528f05d62b94705ddfc4f258
Dadud <Dadud30@gmail.com>
2026-04-15 12:15:47 -0500

    fix(api): add prisma type decoration to FastifyInstance

--- scanner-api/src/plugins/database.ts
+++ scanner-api/src/plugins/database.ts
@@ -1,6 +1,12 @@
 import { FastifyPluginAsync } from 'fastify';
 import { PrismaClient } from '@prisma/client';
 
+declare module 'fastify' {
+  interface FastifyInstance {
+    prisma: PrismaClient;
+  }
+}
+
 export const prismaPlugin: FastifyPluginAsync = async (fastify) => {
   const prisma = new PrismaClient();
   await prisma.$connect();
