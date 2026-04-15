fdee14abe5993ee832575273b38b7707ee2c82db
Dadud <Dadud30@gmail.com>
2026-04-15 10:21:16 -0500

    Update scanner-ui/tailwind.config.js

--- scanner-ui/tailwind.config.js
+++ scanner-ui/tailwind.config.js
@@ -1,17 +1,4 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        scanner: {
-          fire: '#ff4444',
-          police: '#4444ff',
-          ems: '#44ff44',
-          dark: '#1a1a2e',
-          light: '#16213e'
-        }
-      }
-    }
-  },
-  plugins: []
+  theme: { extend: { colors: { scanner: { dark: '#1a1a2e', light: '#16213e' } } } }
 };
